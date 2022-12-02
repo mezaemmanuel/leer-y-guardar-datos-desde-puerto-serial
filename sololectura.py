7a8d8b9db59dbbd80a81e5b556aa3e1c30a5a0a4
Emmanuel Meza <68042653+mezaemmanuel@users.noreply.github.com>
2022-12-02 08:38:54 -0600

    Create sololectura.py

---
+++ sololectura.py
@@ -0,0 +1,6 @@
+import serial
+from serial import Serial # la libreria usada es pyserial
+
+
+while True: # MIENTRAS EL ROGRAMA ESTE CORRIENDO SEGUIRA LEYENDO EL COM A MANERA DE CICLO
+    print(data)
